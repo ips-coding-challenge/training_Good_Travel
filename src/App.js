065ad6a8f7b681d8e0bae254ae32fc7de7b28ccb
myself <android.programmation@laposte.net>
2020-07-23 11:32:06 +0200

    Ready to deploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="fixed inset-0 flex flex-col md:flex-row bg-lightgreen overflow-y-auto md:overflow-hidden">
+    <div className="fixed inset-0 bg-lightgreen overflow-y-auto md:overflow-hidden">
       <Home />
     </div>
   );
